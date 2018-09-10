05dc5fd7719e3d1e8d9b4b603a5e0473328d5dcb
Mo <midbid@roc-dev.com>
2018-09-10 11:16:33 +0200

    -

--- winkelwagentje.js
+++ winkelwagentje.js
@@ -1,14 +1,28 @@
-var obj = document.getElementById("aantal");
-obj.addEventListener("change", berekenSubTotaal)
-
-var prijsinput = document.getElementById("motor price");
-var aantalinput = document.getElementById("aantal");
-var subtotaal = document.getElementById("totaal")
-function berekenSubTotaal(prijsinput, aantalinput, subtotaal) {
-    var prijs = parseFloat(document.getElementById("motor price").textContent) * 1000;
-    var aantal = parseInt(document.getElementById("aantal").value);
+function berekenSubTotaal(a, b, c) {
+    var prijs = parseFloat(a.textContent) * 1000;
+    var aantal = parseInt(b.value);
     var totaalprijs = prijs * aantal;
-    subtotaal.textContent = totaalprijs.toFixed();
+    c.textContent = totaalprijs.toFixed();
 }
 
-berekenSubTotaal(prijsinput, aantalinput, subtotaal);
+var motor_price = document.getElementById("motor_price");
+console.log(motor_price.textContent);
+var aantal = document.getElementById("aantal");
+var totaal = document.getElementById("testje")
+console.log(document.getElementById("testje")
+);
+
+
+aantal.addEventListener("change", function () {
+    berekenSubTotaal(motor_price, aantal, totaal);
+});
+
+var motor1_price = document.getElementById("motor1_price");
+var aantal1 = document.getElementById("aantal1");
+var totaal1 = document.getElementById("totaal1")
+
+
+aantal1.addEventListener("change", function () {
+    berekenSubTotaal(motor1_price, aantal1, totaal1);
+});
+
