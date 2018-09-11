c974bf9fe8702f79810a9170519548ffa4c8ef63
Mo <midbid@roc-dev.com>
2018-09-11 09:53:20 +0200

    -

--- winkelwagentje.js
+++ winkelwagentje.js
@@ -10,7 +10,6 @@ var aantal = document.getElementById("aantal");
 var totaal = document.getElementById("testje");
 
 
-
 aantal.addEventListener("change", function () {
     berekenSubTotaal(motor_price, aantal, totaal);
 });
@@ -23,24 +22,30 @@ var totaal1 = document.getElementById("totaal1");
 aantal1.addEventListener("change", function () {
     berekenSubTotaal(motor1_price, aantal1, totaal1);
 });
+
 var motor2_price = document.getElementById("motor2_price");
 var aantal2 = document.getElementById("aantal2");
 var totaal2 = document.getElementById("totaal2");
 
 
-aantal2.addEventListener("change", function() {
+aantal2.addEventListener("change", function () {
     berekenSubTotaal(motor2_price, aantal2, totaal2);
 });
 
-var totaalprijs = (parseFloat(totaal.textContent)
-+ parseFloat(totaal1.textContent)
-+ parseFloat(totaal2.textContent));
-
-var volletank = 0;
-if(document.getElementById("benzine").checked) {
-    totaalprijs = (totaalprijs + 50).toFixed(2);
-}
-
-totaalknop.addEventListener("click", function(){
+totaalknop.addEventListener("click", function () {
     document.getElementById("totaalprijs").textContent = totaalprijs;
-})
+    var totaalprijs = (parseFloat(totaal.textContent)
+    + parseFloat(totaal1.textContent)
+    + parseFloat(totaal2.textContent));
+
+    var totaalaantal = parseInt(motor_price.textContent) + parseInt(motor1_price.textContent) + parseInt(motor2_price.textContent);
+    console.log(totaalaantal);
+
+    var volletank = 0;
+    if (document.getElementById("benzine").checked) {
+        volletank = 50 + totaalaantal;
+    }
+    document.getElementById("totaalprijs").textContent = (totaalprijs + volletank).toFixed(2);
+
+});
+
